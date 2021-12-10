2f46fddd71245ae1949579c095c4a8a724cfcec2
Kiran Pandit <kiran@makenotion.com>
2021-12-10 13:18:56 -0800

    Revert "Add webhooks path"
    This reverts commit f5d5e0105b788d75f852c976a3575479637f6d95.

--- api/index.js
+++ api/index.js
@@ -3,4 +3,4 @@ const { createNodeMiddleware, createProbot } = require("probot");
 
 const appFn = require("./app"); // this is where we import the module.exports
 const probot = createProbot();
-module.exports = createNodeMiddleware(appFn, { probot, webhooksPath: '/api' });
+module.exports = createNodeMiddleware(appFn, { probot});
