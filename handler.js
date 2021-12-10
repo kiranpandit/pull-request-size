ed3cd603be42e5f2beead30f7c4832744b7f27e5
Kiran Pandit <kiran@makenotion.com>
2021-12-10 10:28:28 -0800

    Revert all the changes

---
+++ handler.js
@@ -0,0 +1,23 @@
+const { createNodeMiddleware, createProbot } = require("probot");
+
+
+const appFn = require("./"); // this is where we import the module.exports
+const probot = createProbot();
+
+module.exports = createNodeMiddleware(appFn, { probot});
+
+// module.exports.webhooks = createLambdaFunction(appFn, {
+//   probot: createProbot(),
+// });
+
+// This is copied from probot hello world
+// const { createNodeMiddleware, createProbot } = require("probot");
+
+// const app = require("../../../app");
+// const probot = createProbot();
+
+// module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
+
+
+
+// this is where we actually want to have some kind of API/listener something
