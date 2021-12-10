4648c641bffa1c606b81c4419eabe803c0c96907
Kiran Pandit <kiran@makenotion.com>
2021-12-09 16:57:49 -0800

    Rename file to index.js

--- handler.js
+++
@@ -1,7 +0,0 @@
-const { createNodeMiddleware, createProbot } = require("probot");
-
-
-const appFn = require("./"); // this is where we import the module.exports
-const probot = createProbot();
-
-module.exports = createNodeMiddleware(appFn, { probot, webhooksPath: '/api/github/webhooks' });
