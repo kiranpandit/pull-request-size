31954cd96477121bcead23dfc61556f1d206a550
Kiran Pandit <kiran@makenotion.com>
2021-12-10 13:28:42 -0800

    Move app outside of api folder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 
 
-const appFn = require("./app"); // this is where we import the module.exports
+const appFn = require("../app"); // this is where we import the module.exports
 const probot = createProbot();
 module.exports = createNodeMiddleware(appFn, { probot});
