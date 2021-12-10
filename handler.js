f4a00bac22958d537761770750b5edaa56a18503
Kiran Pandit <kiran@makenotion.com>
2021-12-09 16:53:34 -0800

    Fix typo

--- handler.js
+++ handler.js
@@ -4,4 +4,4 @@ const { createNodeMiddleware, createProbot } = require("probot");
 const appFn = require("./"); // this is where we import the module.exports
 const probot = createProbot();
 
-module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
+module.exports = createNodeMiddleware(appFn, { probot, webhooksPath: '/api/github/webhooks' });
