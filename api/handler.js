c5c842df0dab26b842ac60a19d9f1bd789e51ca3
Kiran Pandit <kiran@makenotion.com>
2021-12-10 10:35:57 -0800

    Moved index.js into api folder

--- api/handler.js
+++ api/handler.js
@@ -1,7 +1,7 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 
 
-const appFn = require(".."); // this is where we import the module.exports
+const appFn = require("."); // this is where we import the module.exports
 const probot = createProbot();
 
 module.exports = createNodeMiddleware(appFn, { probot});
